baeef4db94a4cc07d0491bd7069a7ee88bc4f3a0
steffent1493 <steffent1493@gmail.com>
2021-06-29 15:05:09 -0400

    mission accomplished

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -8,7 +8,7 @@ function BlogPost() {
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-420" />
     </div>
   );
 }
